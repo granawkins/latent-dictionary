d2e2818d44b410166a60b9fe5f23033f8014a201
granawkins <granthawkins88@gmail.com>
2023-10-22 14:33:10 +0700

    server returns pca instead of full embedding

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify
 from lib.embeddings import get_embeddings
 from lib.oxford_3000 import oxford_3000
 from lib.related_words import get_related_words
+from lib.pca import get_pca, get_default_pca
 
 app = Flask(__name__)
 
@@ -14,7 +15,8 @@ def index():
 def get_oxford_3000():
     words = oxford_3000()
     embeddings = get_embeddings(words)
-    result = {word: embedding for word, embedding in zip(words, embeddings)}
+    pcas = get_default_pca(embeddings, 3)
+    result = {word: list(pca) for word, pca in zip(words, pcas)}
     return jsonify(result)
 
 @app.route('/api/search/<word>')
@@ -22,7 +24,8 @@ def search(word):
     related_words = get_related_words(word)
     words = [word] + related_words
     embeddings = get_embeddings(words)
-    result = {word: embedding for word, embedding in zip(words, embeddings)}
+    pcas = get_pca(embeddings, 3)
+    result = {word: list(pca) for word, pca in zip(words, pcas)}
     return jsonify(result)
 
 if __name__ == '__main__':
