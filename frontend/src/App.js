db01f23bc210f3c2214ffb343bced78c8406a058
granawkins <granthawkins88@gmail.com>
2023-10-22 12:53:16 +0700

    add basic threejs frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import Box from './Box'
+
+const App = () => {
+    return (
+        <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Box position={[-1.2, 0, 0]} />
+            <Box position={[1.2, 0, 0]} />
+        </Canvas>
+    )
+}
+
+export default App
---
+++ frontend/src/Box.js
@@ -0,0 +1,27 @@
+import React, { useRef, useState } from 'react'
+import {useFrame } from '@react-three/fiber'
+
+function Box(props) {
+    // This reference will give us direct access to the mesh
+    const meshRef = useRef()
+    // Set up state for the hovered and active state
+    const [hovered, setHover] = useState(false)
+    const [active, setActive] = useState(false)
+    // Subscribe this component to the render-loop, rotate the mesh every frame
+    useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+    // Return view, these are regular three.js elements expressed in JSX
+    return (
+      <mesh
+        {...props}
+        ref={meshRef}
+        scale={active ? 1.5 : 1}
+        onClick={(event) => setActive(!active)}
+        onPointerOver={(event) => setHover(true)}
+        onPointerOut={(event) => setHover(false)}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      </mesh>
+    )
+  }
+  
+export default Box
