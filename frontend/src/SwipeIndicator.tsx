7454cf2ab768b1de4119244a1da51894442de4a1
MentatBot <160964065+MentatBot@users.noreply.github.com>
2025-01-23 06:47:38 +0000

    Replace custom SVG with FontAwesome icon
    - Switched to FontAwesome's hand-point-right icon
    - Added horizontal swipe animation to make the gesture more obvious
    - Maintained existing timing and fade effects

--- frontend/src/SwipeIndicator.tsx
+++ frontend/src/SwipeIndicator.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHandPointRight } from "@fortawesome/free-solid-svg-icons";
 import "./SwipeIndicator.css";
 
 interface SwipeIndicatorProps {
@@ -27,24 +29,10 @@ const SwipeIndicator: React.FC<SwipeIndicatorProps> = ({ show }) => {
 
   return (
     <div className="swipe-indicator">
-      <svg
-        width="64"
-        height="64"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        {/* Hand with finger */}
-        <path d="M9 11.5V5a2 2 0 0 1 4 0v6.5" />
-        <path d="M9 13.5V12h4v1.5" />
-        <path d="M7 13.5a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2V16a6 6 0 0 1-6 6h0a6 6 0 0 1-6-6v-2.5z" />
-        {/* Motion arrows */}
-        <path d="M17 10l2 2-2 2" />
-        <path d="M19 12H9" />
-      </svg>
+      <FontAwesomeIcon
+        icon={faHandPointRight}
+        style={{ fontSize: "3em" }}
+      />
     </div>
   );
 };
