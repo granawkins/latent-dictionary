aeea8a8344ee51c2b6820ea0995d032b337bcc3a
Grant Hawkins <granthawkins88@gmail.com>
2025-01-23 12:38:59 +0700

    pyright fix

--- backend/db.py
+++ backend/db.py
@@ -18,11 +18,8 @@ load_dotenv()
 DB_PATH = Path("~/.latentdictionary").expanduser()
 DB_PATH.parent.mkdir(exist_ok=True)
 openai_api_key = os.getenv("OPENAI_API_KEY")
+client = chromadb.PersistentClient(path=DB_PATH.as_posix())
 if openai_api_key:
-    client = chromadb.PersistentClient(
-        path=DB_PATH.as_posix(),
-        api_key=openai_api_key,
-    )
     embedding_function = OpenAIEmbeddingFunction(
         api_key=openai_api_key, model_name="text-embedding-3-small"
     )
@@ -30,7 +27,6 @@ if openai_api_key:
         name="latent-dictionary", embedding_function=embedding_function
     )
 else:
-    client = chromadb.PersistentClient(path=DB_PATH.as_posix())
     collection = client.get_or_create_collection(name="latent-dictionary")
 
 
