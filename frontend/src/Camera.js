919501312357c711e41e82aba9107bc97b5910ad
Grant Hawkins <granthawkins88@gmail.com>
2025-01-23 10:44:23 +0700

    fix camera start position

--- frontend/src/Camera.js
+++ frontend/src/Camera.js
@@ -4,20 +4,24 @@ import { useEffect, useState, useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { SCALE } from './Dot';
 
+const DEFAULT_TARGET = {x: 1.5, y: -0.5, z: 11.5}
+
 const Camera = ({ selectedCorpus }) => {
 
     // When selectedCorpus updates, spin/move the camera to get all selected words in view
     const controlsRef = useRef()
-    const [targetCoordinates, setTargetCoordinates] = useState(null)
+    const [targetCoordinates, setTargetCoordinates] = useState(DEFAULT_TARGET)
     const [lookAtPosition, setLookAtPosition] = useState(null)
     useEffect(() => {
         if (selectedCorpus) {
+            if (Object.keys(selectedCorpus).length === 1) {
+                return;
+            }
             // 1. Compute the bounding box
             const bbox = new THREE.Box3();
             Object.values(selectedCorpus).forEach(dot => {
                 bbox.expandByPoint(new THREE.Vector3(dot.x * SCALE, dot.y * SCALE, dot.z * SCALE));
             });
-
             // 2. Calculate the center point
             const center = bbox.getCenter(new THREE.Vector3());
             setLookAtPosition(center);
@@ -31,6 +35,13 @@ const Camera = ({ selectedCorpus }) => {
             const target = new THREE.Vector3(center.x, center.y, center.z + cameraDistance);
             if (Object.keys(selectedCorpus).length === 1) {
                 target.z += 5;
+            }
+            if (target.z === 0 && target.y === 0 && target.x === 0) {
+                return
+            }
+            if (target !== targetCoordinates) {
+                console.log("target", target)
+                console.log("targetCoordinates", targetCoordinates)
             }
             setTargetCoordinates(target);
         }
